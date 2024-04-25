5ca8affb8afc322306a51a3e9d38fd5dea15c8ce
Yunfei Tang <yunfeitang743@gmail.com>
2024-04-25 14:37:22 -0400

    delete some blank files

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {userData} from '../data/index.js';
-import validation from '../validation.js';
-
-const router = Router();
-//TODO: design our own router layer for posts;
-
-
-export default router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {userData} from '../data/index.js';
-import validation from '../validation.js';
-
-const router = Router();
-//TODO:implementations below
-
-
-export default router;
