a9384263ddfeb067bfffcb020f9e0ad786bae3ba
Yunfei Tang <yunfeitang743@gmail.com>
2024-05-01 19:26:19 -0400

    add about page

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ import signupRoutes from "./signup-routes.js";
 import logoutRoutes from "./logout-routes.js";
 import dashboardRoutes from "./dashboard-routes.js";
 import editRoutes from "./edit-routes.js";
+import aboutRoutes from "./about-routed.js";
 
 //use each of the routes files in the routes folder
 router.use("/", indexRoutes);
@@ -19,6 +20,7 @@ router.use("/signup", signupRoutes);
 router.use("/logout", logoutRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/edit", editRoutes);
+router.use("/about", aboutRoutes);
 router.use("*", (req, res) => {
   res.sendStatus(404);
 });
